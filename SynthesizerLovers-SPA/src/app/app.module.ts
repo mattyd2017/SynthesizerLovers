ec2f9d70f347e3a91cb12f539dfc76a67333b9ce
mattyd2017 <32998214+mattyd2017@users.noreply.github.com>
2019-01-09 15:54:11 +0000

    angular routing and areas for memebers

--- SynthesizerLovers-SPA/src/app/app.module.ts
+++ SynthesizerLovers-SPA/src/app/app.module.ts
@@ -11,6 +11,12 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 @NgModule({
@@ -18,17 +24,22 @@ import { RegisterComponent } from './register/register.component';
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
       AlertifyService,
+      AuthGuard,
       ErrorInterceptorProvider
    ],
    bootstrap: [
