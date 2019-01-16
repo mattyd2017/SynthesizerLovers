9d1539ee15e7fa89c133c7b689332a8d707e24ec
mattyd2017 <32998214+mattyd2017@users.noreply.github.com>
2019-01-16 21:07:32 +0000

    completed registration

--- SynthesizerLovers-SPA/src/app/app.module.ts
+++ SynthesizerLovers-SPA/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { AlertifyService } from './_services/alertify.service';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AuthService } from './_services/auth.service';
-import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, BsDatepickerModule } from 'ngx-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
-import { FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
@@ -51,7 +51,9 @@ export function tokenGetter() {
       BrowserModule,
       HttpClientModule,
       FormsModule,
+      ReactiveFormsModule,
       TabsModule.forRoot(),
+      BsDatepickerModule.forRoot(),
       BsDropdownModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       NgxGalleryModule,
--- SynthesizerLovers.API/Helpers/AutoMapperProfiles.cs
+++ SynthesizerLovers.API/Helpers/AutoMapperProfiles.cs
@@ -23,6 +23,7 @@ namespace SynthesizerLovers.API.Helpers
            CreateMap<UserForUpdateDto, User>();
            CreateMap<Photo, PhotoForReturnDto>();
            CreateMap<PhotoForCreationDto, Photo>();
+           CreateMap<UserForRegisterDto, User>();
        } 
     }
 }
