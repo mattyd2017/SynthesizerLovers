fa7a405077111cae5adc8496797f0ef9922df693
mattyd2017 <32998214+mattyd2017@users.noreply.github.com>
2019-01-19 22:29:38 +0000

    messaging system implemented and final commit

--- SynthesizerLovers-SPA/src/app/members/member-detail/member-detail.component.ts
+++ SynthesizerLovers-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,9 +1,11 @@
 import { AlertifyService } from './../../_services/alertify.service';
 import { User } from './../../_models/user';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from 'src/app/_services/user.service';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { TabsetComponent } from 'ngx-bootstrap';
+
 
 
 @Component({
@@ -12,6 +14,7 @@ import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gal
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs') memberTabs: TabsetComponent;
   user: User;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
@@ -22,6 +25,11 @@ export class MemberDetailComponent implements OnInit {
    this.route.data.subscribe(data => {
      this.user = data['user'];
    });
+   this.route.queryParams.subscribe(params => {
+     const selectedTab = params['tab'];
+     this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true;
+   });
+
    this.galleryOptions = [
      {
        width: '500px',
@@ -45,5 +53,9 @@ export class MemberDetailComponent implements OnInit {
       });
     }
     return imageUrls;
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
   }
 }
---
+++ SynthesizerLovers.API/Helpers/MessageParams.cs
@@ -0,0 +1,20 @@
+namespace SynthesizerLovers.API.Helpers
+{
+    public class MessageParams
+    {
+        private const int _maxPageSize = 50;
+        private int _pageSize = 10;
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = (value > _maxPageSize) ? _maxPageSize : value; }
+        }
+
+        public int UserId { get; set; }
+
+        public string MessageContainer { get; set; } = "Unread";
+    }
+}
